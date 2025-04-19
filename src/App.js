fcec5d698c4f5c7e7d838a8e6306220891691fb2
Sourav Bera <sbera987654321@gmail.com>
2025-04-19 22:25:22 +0530

    Final push ~ I think

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUp from "./components/SignUp";
-import LogIn from "./components/LogIn";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import SignUp from "./components/SignUp";
+// import LogIn from "./components/LogIn";
+import HomePage from "./components/Shop Owner Dashboard/HomePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<SignUp />} />
+    //     <Route path="/login" element={<LogIn />} />
+    //   </Routes>
+    // </Router>
+    <HomePage />
   );
 }
 
